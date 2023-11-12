8c464bb1706b9a9b94774f49a0d9c4bc5422d132
devudubey15 <51876969+devudubey15@users.noreply.github.com>
2023-11-12 23:15:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+namespace ASP_CORE_APPILACTION
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//CreateBuilder is the static method of WebApplication class which returns the instance of WebApplicationBuilder class
+			//which is then assigned to builder . which will used to configure services, middleware and other aspect of application.
+			var builder = WebApplication.CreateBuilder(args);
+
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+
+
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+			//app.Run(async (context) => { await context.Response.WriteAsync("ASP .net core "); }
+			//	);
+
+
+			app.Run();
+		}
+	}
+}
